0f6299890f5574c2533b4481de4854cb21ee45db
Nicholas Lai <nicholaslhlai@gmail.com>
2022-03-25 19:18:31 +0000

    TradingStrategy.java created online with Bitbucket

---
+++ src/main/java/TradingStrategy.java
@@ -0,0 +1,15 @@
+
+public class TradingStrategy {
+	
+	public TradingStrategy(String name, float starting, float upper, float lower, String Method) {
+		
+	}
+	
+	public TradingStrategy getStrategy(String strategyName) {
+		
+	}
+	
+	public Boolean evaluateStrategy(TradingStrategy strategy) {
+		
+	}
+}
