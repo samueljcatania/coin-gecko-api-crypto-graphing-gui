e111808bd04f5c8ead119f61a325bd6cc277e5ae
SamuelCatania <69449284+SamuelCatania@users.noreply.github.com>
2022-04-03 23:29:04 -0400

    Created login GUI interface
    Signed-off-by: SamuelCatania <69449284+SamuelCatania@users.noreply.github.com>

---
+++ src/main/java/Login.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login implements ActionListener {
+
+    public static void main(String[] args){
+         new Login();
+    }
+
+    public Login(){
+        JFrame frame = new JFrame("Login");
+        frame.setSize(350,125);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+
+        JTextField usernameField = new JTextField(20);
+        JPasswordField passwordField = new JPasswordField(20);
+
+        JLabel username = new JLabel("Username:");
+        JLabel password = new JLabel("Password:");
+        JButton submit = new JButton("Submit!");
+
+        panel.add(username);
+        username.setLabelFor(usernameField);
+        panel.add(usernameField);
+
+        panel.add(password);
+        password.setLabelFor(passwordField);
+        panel.add(passwordField);
+
+        panel.add(submit);
+
+        //Display
+        frame.setContentPane(panel);
+        frame.setVisible(true);
+    }
+
+
+    /**
+     * Invoked when an action occurs.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+
+}
