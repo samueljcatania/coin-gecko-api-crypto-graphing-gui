6d16979ac66b3855142cfb539c9647c29cb98f3c
Samuel CatanIa <scatani9@uwo.ca>
2022-04-04 23:50:15 -0400

    Completed backend for Login.java (added .json file to store credentials) and fixed bugs within Login.java.
    Signed-off-by: SamuelCatania <69449284+SamuelCatania@users.noreply.github.com>

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -1,21 +1,24 @@
+import com.google.gson.*;
+
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class Login implements ActionListener {
+public class Login {
 
-    JTextField usernameField, passwordField;
+    private JTextField usernameField, passwordField;
+    private JFrame loginFrame;
 
-    public static void main(String[] args){
-         new Login();
+    public static void main(String[] args) {
+        new Login();
     }
 
-    public Login(){
-        JFrame frame = new JFrame("Login");
-        frame.setSize(350,125);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public Login() {
+        loginFrame = new JFrame("Login");
+        loginFrame.setSize(350, 125);
+        loginFrame.setResizable(false);
+        loginFrame.setLocationRelativeTo(null);
+        loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel();
 
@@ -24,7 +27,12 @@ public class Login implements ActionListener {
 
         JLabel username = new JLabel("Username:");
         JLabel password = new JLabel("Password:");
-        JButton submit = new JButton("Submit!");
+
+        JButton submit = new JButton("Submit");
+
+        submit.addActionListener(l -> {
+            checkPassword();
+        });
 
         panel.add(username);
         username.setLabelFor(usernameField);
@@ -36,26 +44,43 @@ public class Login implements ActionListener {
 
         panel.add(submit);
 
-        //Display
-        frame.setContentPane(panel);
-        frame.setVisible(true);
+        //Display the window
+        loginFrame.setContentPane(panel);
+        loginFrame.setVisible(true);
     }
 
-
-    /**
-     * Invoked when an action occurs.
-     *
-     * @param e the event to be processed
-     */
-    @Override
-    public void actionPerformed(ActionEvent e) {
+    private void checkPassword() {
         String username = usernameField.getText();
         String password = passwordField.getText();
 
-        System.out.println(username);
-        System.out.println(password);
+        boolean validPassword = false;
+
+        JsonParser jsonParser = new JsonParser();
+
+        try (FileReader reader = new FileReader("src/main/text/credentials.json")) {
+
+            Object object = jsonParser.parse(reader);
+
+            JsonArray jsonArray = (JsonArray) object;
+
+            for (int i = 0; i < jsonArray.size(); i++) {
+                JsonObject jsonObject = (JsonObject) jsonArray.get(i);
+
+                if (jsonObject.get("username").getAsString().equals(username) && jsonObject.get("password").getAsString().equals(password)) {
+                    validPassword = true;
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (validPassword) {
+            System.out.println("The user can enter.");
+        } else {
+            passwordField.setText("");
+            JOptionPane.showMessageDialog(loginFrame, "Invalid Password");
+        }
 
     }
-
-
 }
--- src/main/java/TradingStrategy.java
+++ src/main/java/TradingStrategy.java
@@ -6,90 +6,89 @@ import java.util.Date;
  * This class represents a trading strategy. It includes the name of the strategy, the coin in interest,
  * the current price of the coin, the target price of the coin, and the action it will take.
  */
-public class TradingStrategy implements TradingStrategyInterface{
+public class TradingStrategy implements TradingStrategyInterface {
 
-	private String strategyName;
-	private String[] names;
-	private String[] symbols;
-	private int[] prices;
-	private String coinTarget;
-	private String action;
-	private int quantity;
+    private String strategyName;
+    private String[] names;
+    private String[] symbols;
+    private int[] prices;
+    private String coinTarget;
+    private String action;
+    private int quantity;
 
-	public TradingStrategy(String strategyName, String coinTarget, String action, int quantity, String[] names,
-						   String[] symbols, int[] prices) {
-		this.strategyName = strategyName;
-		this.names = names;
-		this.symbols = symbols;
-		this.prices = prices;
-		this.coinTarget = coinTarget;
-		this.action = action;
-		this.quantity = quantity;
-	}
+    public TradingStrategy(String strategyName, String coinTarget, String action, int quantity, String[] names,
+                           String[] symbols, int[] prices) {
+        this.strategyName = strategyName;
+        this.names = names;
+        this.symbols = symbols;
+        this.prices = prices;
+        this.coinTarget = coinTarget;
+        this.action = action;
+        this.quantity = quantity;
+    }
 
-	@Override
-	public String[] trade(String[] coinList, double[] coinPriceList) {
-		DateFormat dateFormat = new SimpleDateFormat("dd-MMMM-yyyy");
-		Date date = new Date();
+    @Override
+    public String[] trade(String[] coinList, double[] coinPriceList) {
+        DateFormat dateFormat = new SimpleDateFormat("dd-MMMM-yyyy");
+        Date date = new Date();
 
-		for(int i = 0; i < names.length; i++){
-			int index = -1;
+        for (int i = 0; i < names.length; i++) {
+            int index = -1;
 
-			for(int x = 0; x < coinList.length; x++){
-				if(coinList[x].equalsIgnoreCase(names[i])){
-					index = x;
-				}
-			}
+            for (int x = 0; x < coinList.length; x++) {
+                if (coinList[x].equalsIgnoreCase(names[i])) {
+                    index = x;
+                }
+            }
 
-			if(index != -1) {
-				boolean evaluation = true;
+            if (index != -1) {
+                boolean evaluation = true;
 
-				switch (symbols[i]) {
+                switch (symbols[i]) {
 
-					case "<":
+                    case "<":
 
-						if (!(prices[i] < coinPriceList[index])){
-							evaluation = false;
-						}
-						break;
+                        if (!(prices[i] < coinPriceList[index])) {
+                            evaluation = false;
+                        }
+                        break;
 
-					case ">":
-						if (!(prices[i] > coinPriceList[index])){
-							evaluation = false;
-						}
-						break;
+                    case ">":
+                        if (!(prices[i] > coinPriceList[index])) {
+                            evaluation = false;
+                        }
+                        break;
 
-					case "=":
-						if (!(prices[i] == coinPriceList[index])){
-							evaluation = false;
-						}
-						break;
+                    case "=":
+                        if (!(prices[i] == coinPriceList[index])) {
+                            evaluation = false;
+                        }
+                        break;
 
-					case ">=":
-						if (!(prices[i] >= coinPriceList[index])){
-							evaluation = false;
-						}
-						break;
+                    case ">=":
+                        if (!(prices[i] >= coinPriceList[index])) {
+                            evaluation = false;
+                        }
+                        break;
 
-					case "<=":
-						if (!(prices[i] <= coinPriceList[index])){
-							evaluation = false;
-						}
-						break;
+                    case "<=":
+                        if (!(prices[i] <= coinPriceList[index])) {
+                            evaluation = false;
+                        }
+                        break;
 
-				}
+                }
 
-				if(!evaluation){
-					return new String[]{strategyName, coinTarget, "Fail", "Null", "Null", dateFormat.format(date)};
-				}
+                if (!evaluation) {
+                    return new String[]{strategyName, coinTarget, "Fail", "Null", "Null", dateFormat.format(date)};
+                }
 
-			}
-			else{
-				return new String[]{strategyName, coinTarget, "Fail", "Null", "Null", dateFormat.format(date)};
-			}
+            } else {
+                return new String[]{strategyName, coinTarget, "Fail", "Null", "Null", dateFormat.format(date)};
+            }
 
-		}
+        }
 
-		return new String[]{strategyName, coinTarget, action, quantity+"", "", dateFormat.format(date)};
-	}
+        return new String[]{strategyName, coinTarget, action, quantity + "", "", dateFormat.format(date)};
+    }
 }
